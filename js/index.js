0894ffe66eb2743b5cb54b94e670201cde8d6d8d
CydYrg <ceyda_yurga@hotmail.com>
2021-05-04 22:15:49 +0100

    mouseover mouseleave click dbclick done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+const header = document.querySelector('.main-navigation');
+
+header.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'yellow';
+    event.stopPropogation();
+});
+
+header.addEventListener('mouseleave', (event) => {
+    event.target.style.backgroundColor = 'initial';
+});
+
+Array.from(document.images).forEach (function (image) {
+    image.addEventListener('mouseover', (event) => {
+        event.target.style.boxShadow = '5px 5px 25px pink';
+    });
+});
+
+Array.from (document.images).forEach (function (image) {
+    image.addEventListener ('mouseleave', (event) => {
+        event.target.style.boxShadow = 'initial';
+    });
+});
+
+const busImg = document.querySelector('.intro img');
+
+busImg.addEventListener('click',function(){
+  document.body.style.backgroundColor = 'coral';
+});
+
+busImg.addEventListener('dbclick', function(){
+    document.body.style.backgroundColor = 'initial';
+});
